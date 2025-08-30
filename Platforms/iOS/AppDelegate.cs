ddc5b228cf4fcf4e3c2612c175f0b1223ae1f767
Michael05005 <kirymich@gmail.com>
2025-08-30 19:02:23 +0200

    Add files via upload

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PA_CPR_UI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
